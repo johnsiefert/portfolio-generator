879daf3065a35fbad2978420323fa809de2ff673
johnsiefert <johnsief12@gmail.com>
2021-11-23 18:18:52 -0500

    added HTML

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
+const fs = require('fs');
+
+const generatePage = require('./src/page-template');
+
 const profileDataArgs = process.argv.slice(2);
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+console.log(profileDataArgs);
 
-  console.log('================');
+const [John, johnsiefert] = profileDataArgs;
 
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+console.log(John, johnsiefert);
 
-printProfileData(profileDataArgs);
+const pageHTML = generatePage(John, johnsiefert);
+
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
